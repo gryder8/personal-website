0f05c540a08a9f0a7b79245441dcc94c41fef26d
gryder8 <gavin.ryder82@gmail.com>
2021-11-02 12:58:41 -0700

    add layout

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+import Layout from './layout';
+const AboutPage = () => {
+    return (
+        <Layout pageTitle = "About Me">
+            <p>This is my personal website, built with Gatsby</p>
+        </Layout>
+    )
+}
+
+export default AboutPage;
