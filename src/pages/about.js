5ec8a25b3bf045d45bf4cf439335af5e8386a0c0
gryder8 <gavin.ryder82@gmail.com>
2021-11-02 13:20:29 -0700

    reorg

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
-import Layout from './layout';
+import Layout from '../components/layout';
 const AboutPage = () => {
     return (
         <Layout pageTitle = "About Me">
